55fd53d3abe42c394296f73bc9b054155b656f14
mikemuturi <mikeirungumuturi@gmail.com>
2024-05-30 12:42:50 +0300

    Database connection

--- lib/server.dart
+++ lib/server.dart
@@ -1,8 +1,16 @@
 import 'package:mongo_dart/mongo_dart.dart';
 
-main(List<String> arguements) async {
-  Db db = await Db.create(
-      'mongodb+srv://Mike:Mike_1234@dart.gsx83ml.mongodb.net/?retryWrites=true&w=majority&appName=Dart');
-  await db.open();
-  print('connected to database');
+void main(List<String> arguments) async {
+  final db = Db(
+      'mongodb+srv://Mike:12345@dart.gsx83ml.mongodb.net/?retryWrites=true&w=majority&appName=Dart');
+
+  try {
+    await db.open();
+    print('Connected to the database');
+    // Your further database operations can go here
+  } catch (e) {
+    print('Error connecting to the database: $e');
+  } finally {
+    await db.close();
+  }
 }
